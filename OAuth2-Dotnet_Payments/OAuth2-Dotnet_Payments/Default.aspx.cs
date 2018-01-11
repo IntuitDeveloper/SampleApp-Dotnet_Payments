5308e1739dc8bedfa4153208496c15271a1b6964
nbhambhani <nikita_bhambhani@intuit.com>
2018-01-11 12:25:16 -0800

    Remove old code for Charge endpoint

--- OAuth2-Dotnet_Payments/OAuth2-Dotnet_Payments/Default.aspx.cs
+++ OAuth2-Dotnet_Payments/OAuth2-Dotnet_Payments/Default.aspx.cs
@@ -1067,14 +1067,12 @@ namespace OAuth2_Dotnet_UsingSDK
             string uri = paymentsBaseUrl + cardChargeEndpoint;
 
             // build the request
-            //string cardChargeRequestBody = "{\"amount\": \"10.55\",\"token\":\""+ cardToken+ "\",\"currency\": \"USD\"}";
             string cardChargeRequestBody= "{\"amount\":\"10.55\",\"token\":\""+ cardToken + "\",\"currency\":\"USD\",\"context\":{\"mobile\":\"false\",\"isEcommerce\":\"true\"}}";
             
             // send the request
             HttpWebRequest cardChargeRequest = (HttpWebRequest)WebRequest.Create(uri);
             cardChargeRequest.Method = "POST";
             cardChargeRequest.Headers.Add(string.Format("Authorization: Bearer {0}", access_token));
-            //cardChargeRequest.Headers.Add(string.Format("Authorization: Bearer {0}", "23423"));
             cardChargeRequest.ContentType = "application/json";
             cardChargeRequest.Accept = "application/json";
             cardChargeRequest.Headers.Add("Request-Id", Guid.NewGuid().ToString());//assign unique guid everytime
